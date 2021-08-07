1d606a93d15fdef426a965d72afa9ae1ad693755
jiexilim <jiexilim.ljx@gmail.com>
2021-08-07 11:00:16 +0800

    Update ft_lstadd_back.c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,10 +6,10 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	{
 		if (!(*lst)->next)
 		{
-			*lst->new;
+			(*lst).next->new;
 			return ;
 		}
-		lst = lst->next;
+		*lst = (*lst)->next;
 	}
 	*lst = new;
 }

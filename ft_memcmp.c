0b93ed25ed62b0ea5c2a5753dade80a8519a35b9
jiexilim <jiexilim.ljx@gmail.com>
2021-08-07 19:17:07 +0800

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*s1_ptr;
 	unsigned char	*s2_ptr;
 
-	if (s1 == s2 || n = 0)
+	if (s1 == s2 || n == 0)
 		return (0);
 	s1_ptr = (unsigned char *) s1;
 	s2_ptr = (unsigned char *) s2;

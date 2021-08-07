3bcd79b092b4dbb1e8e40afc378ae55ddb6b15a0
jiexilim <jiexilim.ljx@gmail.com>
2021-08-08 00:26:35 +0800

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -1,22 +1,30 @@
 #include "libft.h"
-
+// #include <stdio.h>
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*newsub;
 	size_t	i;
 
-	if (!s)
-		return (NULL);
-	else if (start >= ft_strlen(s))
-		return ("\0");
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
-	newsub = (char *) malloc(sizeof(*s) * (len - start + 1));
+	// if (!s)
+	// 	return (NULL);
+	// if (len > ft_strlen(s))
+	// 	len = ft_strlen(s);
+	newsub = (char *) malloc(sizeof(*s) * (len + 1));
+	// if (start >= ft_strlen(s))
+	// 	return (newsub);
 	if (!newsub)
 		return (NULL);
 	i = 0;
 	while (s[start] && i < len)
 		newsub[i++] = s[start++];
-	newsub[i] = '\0';
+	newsub[i] = 0;
 	return (newsub);
 }
+
+// int main()
+// {
+// 	printf("%s\n", ft_substr("tripouille", 100, 1));
+// 	printf("%s\n", ft_substr("1", 42, 42000000));
+// 	printf("%s\n", ft_substr("0123456789", 9, 10));
+// 	printf("%s\n", ft_substr("42", 0, 0));
+// }

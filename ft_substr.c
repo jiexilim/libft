391cfad8905214125eab935f5c6d4c8c823ca657
jiexilim <jiexilim.ljx@gmail.com>
2021-08-08 00:39:18 +0800

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -1,46 +1,19 @@
 #include "libft.h"
 // #include <stdio.h>
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	char	*newsub;
-// 	size_t	i;
-
-// 	newsub = (char *) malloc(sizeof(*s) * (len + 1));
-// 	if (!newsub)
-// 		return (newsub);
-// 	i = 0;
-// 	while (s[start] && i < len)
-// 		newsub[i++] = s[start++];
-// 	newsub[i] = 0;
-// 	return (newsub);
-// }
-
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*temp;
-	int		i;
+	char	*newsub;
+	size_t	i;
 
+	newsub = malloc(len + 1);
+	if (!newsub)
+		return (newsub);
 	i = 0;
-	if (s == NULL)
-		return (NULL);
-	if ((size_t)start > len)
-	{
-		if ((temp = malloc(1)) == NULL)
-			return (NULL);
-		temp[0] = '\0';
-		return (temp);
-	}
-	if ((temp = ft_calloc((len + 1), sizeof(char))) == NULL)
-		return (NULL);
-	while (start < (unsigned int)len)
-	{
-		temp[i] = s[start];
-		i++;
-		start++;
-	}
-	return (temp);
+	while (s[start] && i < len)
+		newsub[i++] = s[start++];
+	newsub[i] = 0;
+	return (newsub);
 }
-
 
 // int main()
 // {

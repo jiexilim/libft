4ca772b4ebf403f447dadad28b2db0a72b01db64
jiexilim <jiexilim.ljx@gmail.com>
2021-08-07 19:13:09 +0800

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -9,17 +9,25 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	s1_ptr = (unsigned char *) s1;
 	s2_ptr = (unsigned char *) s2;
 	i = 0;
-	if (!s1_ptr)
-		return (-1);
-	else if (!s2_ptr)
-		return (1);
+	if (!s1_ptr || !s2_ptr)
+		return (NULL);
 	while (i < n)
 	{
-		if (s1_ptr[i] > s2_ptr[i])
-			return (1);
-		else if (s1_ptr[i] < s2_ptr[i])
-			return (-1);
+		if (s1_ptr[i] != s2_ptr[i])
+			return (s1_ptr[i] - s2_ptr[i]);
 		i++;
 	}
 	return (0);
 }
+
+#include <stdio.h>
+int main()
+{
+	char *s1 = "atoms\0\0\0\0";
+	char *s2 = "atoms\0abc";
+	size_t size = 8;
+	printf("%d\n", memcmp(s1, s2, size));
+	printf("%d\n", ft_memcmp(s1, s2, size));
+}
+
+

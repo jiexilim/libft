70584c9e22d174a568c6ebdf2f034ff64bee65cc
jiexilim <jiexilim.ljx@gmail.com>
2021-08-08 16:24:13 +0800

    Update .c files

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -3,7 +3,7 @@
 void	ft_putendl_fd(char const *s, int fd)
 {
 	if (!s)
-		return (NULL);
+		return ;
 	ft_putstr_fd(s, fd);
 	write(fd, "\n", 1);
 }
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -3,7 +3,7 @@
 void	ft_putstr_fd(char const *s, int fd)
 {
 	if (!s)
-		return (NULL);
+		return ;
 	while (*s)
 		write(fd, s++, 1);
 }

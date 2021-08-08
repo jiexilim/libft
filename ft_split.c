95208f187ed6d34c532c8ac1465b02165af5e885
jiexilim <jiexilim.ljx@gmail.com>
2021-08-08 15:46:01 +0800

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -1,5 +1,5 @@
 #include "libft.h"
-
+#include <stdio.h>
 static size_t	wrd_cnt(char *str, char c)
 {
 	size_t	count;
@@ -26,54 +26,46 @@ static void	split(char **s, char c, char ***arr, int *arr_i)
 	int		i;
 
 	start = 0;
-	while (*s[start])
+	while ((*s)[start])
 	{
-		while (*s[start] && *s[start] == c)
+		while ((*s)[start] && (*s)[start] == c)
 			start++;
 		if (start >= ft_strlen(*s))
 			return ;
 		end = start;
-		while (*s[end] && *s[end] != c)
+		while ((*s)[end] && (*s)[end] != c)
 			end++;
-		*arr[*arr_i] = malloc(end - start + 1);
+		(*arr)[*arr_i] = malloc(end - start + 1);
 		i = 0;
 		while (start < end)
-			*arr[*arr_i][i++] = *s[start++];
-		*arr[*arr_i++][i] = '\0';
+			(*arr)[*arr_i][i++] = (*s)[start++];
+		(*arr)[(*arr_i)++][i] = '\0';
 	}
 }
 
 char	**ft_split(char const *s, char c)
 {
-	// size_t	start;
-	// size_t	end;
 	char	**arr;
 	int		arr_i;
-	// int		i;
 
 	if (!s)
 		return (NULL);
 	arr = (char **) malloc(sizeof(char *) * wrd_cnt((char *) s, c) + 1);
 	if (!arr)
 		return (NULL);
-	// start = 0;
 	arr_i = 0;
 	split((char **) &s, c, &arr, &arr_i);
-	// while (s[start])
-	// {
-	// 	while (s[start] && s[start] == c)
-	// 		start++;
-	// 	if (start >= ft_strlen(s))
-	// 		return (arr_app_null(arr, arr_i));
-	// 	end = start;
-	// 	while (s[end] && s[end] != c)
-	// 		end++;
-	// 	arr[arr_i] = malloc(end - start + 1);
-	// 	i = 0;
-	// 	while (start < end)
-	// 		arr[arr_i][i++] = s[start++];
-	// 	arr[arr_i++][i] = '\0';
-	// }
 	arr[arr_i] = NULL;
 	return (arr);
 }
+
+// int main()
+// {
+// 	char **res = ft_split("hahaha", 'a');
+// 	int i = 0;
+// 	while(res[i])
+// 	{
+// 		printf("%s\n", res[i]);
+// 		i++;
+// 	}
+// }

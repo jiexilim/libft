393a33ae26f4e06ca2ef053a91531e6e8653a3ff
jiexilim <jiexilim.ljx@gmail.com>
2021-08-08 17:59:14 +0800

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,72 +1,23 @@
 #include "libft.h"
-// #include <stdio.h>
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
-// 	t_list	*head;
-// 	t_list	*new_ele;
-
-// 	if (!f || !del)
-// 		printf("haha");
-// 	head = NULL;
-// 	while (lst)
-// 	{
-// 		new_ele = ft_lstnew(f(lst->content));
-// 		if (!new_ele)
-// 		{
-// 			ft_lstclear(&head, del);
-// 			return (NULL);
-// 		}
-// 		ft_lstadd_back(&head, new_ele);
-// 		lst = lst->next;
-// 	}
-// 	return (head);
-// }
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_lst;
-	t_list	*new_elem;
+	t_list	*head;
+	t_list	*new_ele;
 
-	if (!lst || !f)
-		return (NULL);
-	if (!(new_elem = ft_lstnew(f(lst->content))))
-	{
-		ft_lstclear(&lst, del);
-		return (NULL);
-	}
-	new_lst = new_elem;
-	lst = lst->next;
+	if (!lst || !del)
+		printf("haha");
+	head = NULL;
 	while (lst)
 	{
-		if (!(new_elem = ft_lstnew(f(lst->content))))
+		new_ele = ft_lstnew(f(lst->content));
+		if (!new_ele)
 		{
-			ft_lstclear(&lst, del);
-			ft_lstclear(&new_lst, del);
-			break ;
+			ft_lstclear(&head, del);
+			return (NULL);
 		}
+		ft_lstadd_back(&head, new_ele);
 		lst = lst->next;
-		ft_lstadd_back(&new_lst, new_elem);
 	}
-	return (new_lst);
+	return (head);
 }
-
-// #include <stdio.h>
-// int main()
-// {
-// 	void *		lstmap_f(void *content) {
-// 		(void) content;
-// 		return ("OK");
-// 	}
-	
-// 	t_list *l = ft_lstnew(strdup("1"));
-// 	t_list *ret;
-// 	l->next = ft_lstnew(strdup("2"));
-// 	(l->next)->next = ft_lstnew(strdup("3"));
-// 	ret = ft_lstmap(l, lstmap_f, ((void *)0));
-// 	// printf("%d\n", strcmp(ret->content, "OK !") && strcmp(ret->next->content, "OK !") 
-// 	// 	&& strcmp(ret->next->next->content, "OK !") && !strcmp(l->content, " 1 2 3 ") 
-// 	// 	&& !strcmp(l->next->content, "ss") && !strcmp(l->next->next->content, "-_-"));
-// 	printf("%s\n", (char *)(l->content));
-// }
-
-	

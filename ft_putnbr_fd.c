8d151cabedd6b13ff5bdc02bab57c97abaa1f7f8
jiexilim <jiexilim.ljx@gmail.com>
2021-08-08 14:05:44 +0800

    Update ft_putnbr.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -2,8 +2,26 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
+	char	c;
 
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
+	if (n == -2147483648)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(8, fd);
+	}
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		ft_putnbr_fd(-n, fd);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		c = n + '0';
+		write(fd, &c, 1);
+	}
 }

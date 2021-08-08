7135c49b1edd1d85cbd0aaf3902d3fb1d51b807c
jiexilim <jiexilim.ljx@gmail.com>
2021-08-08 11:27:12 +0800

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	size_t	s_len;
 
-	s_len = ft_strlen(s)
+	s_len = ft_strlen(s);
 	if (start > len || start > s_len)
 	{
 		newsub = malloc(1);

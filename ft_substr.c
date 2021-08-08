6e2cc2e55876909e2197e212491a897204fbb0c6
jiexilim <jiexilim.ljx@gmail.com>
2021-08-08 14:49:15 +0800

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -1,17 +1,24 @@
 #include "libft.h"
-// #include <stdio.h>
+
+static char	*arr_app_null(char *arr, int i)
+{
+	arr[i] = '\0';
+	return (arr);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*newsub;
 	size_t	i;
 	size_t	s_len;
 
+	if (!s)
+		return (NULL);
 	s_len = ft_strlen(s);
 	if (start > len || start > s_len)
 	{
 		newsub = malloc(1);
-		newsub[0] = '\0';
-		return (newsub);
+		return (arr_app_null(newsub, 0));
 	}
 	else if (len > s_len)
 	{
@@ -20,22 +27,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	else
 		newsub = malloc(len + 1);
-	// printf("%ld\n", len - start + 1);
 	if (!newsub)
 		return (newsub);
 	i = 0;
 	while (s[start] && i < len)
 		newsub[i++] = s[start++];
-	newsub[i] = '\0';
-	return (newsub);
+	return (arr_app_null(newsub, i));
 }
-
-
-// int main()
-// {
-// 	char *str = "i just want this part #############";
-// 	size_t size = 10;
-// 	int ret_size;
-
-// 	printf("%s\n", ft_substr(str, 5, size));
-// }
